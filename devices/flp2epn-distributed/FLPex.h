c9ef7106d7b628d7a6c0dfebf9c08b4b388d2dbb
Alexey Rybalchenko <alexryba@gmail.com>
2014-11-21 17:19:34 +0100

    try no sampler

--- devices/flp2epn-distributed/FLPex.h
+++ devices/flp2epn-distributed/FLPex.h
@@ -26,12 +26,15 @@ class FLPex : public FairMQDevice
       HeartbeatTimeoutInMs,
       NumFLPs,
       SendOffset,
+      EventRate,
+      EventSize,
       Last
     };
 
     FLPex();
     virtual ~FLPex();
 
+    void ResetEventCounter();
     virtual void SetProperty(const int key, const std::string& value, const int slot = 0);
     virtual std::string GetProperty(const int key, const std::string& default_ = "", const int slot = 0);
     virtual void SetProperty(const int key, const int value, const int slot = 0);
@@ -51,6 +54,10 @@ class FLPex : public FairMQDevice
     std::queue<FairMQMessage*> fIdBuffer;
     std::queue<FairMQMessage*> fDataBuffer;
     vector<boost::posix_time::ptime> fOutputHeartbeat;
+
+    int fEventSize;
+    int fEventRate;
+    int fEventCounter;
 };
 
 } // namespace Devices
--- devices/flp2epn-distributed/FLPexSampler.cxx
+++ devices/flp2epn-distributed/FLPexSampler.cxx
@@ -18,9 +18,6 @@ using namespace std;
 using namespace AliceO2::Devices;
 
 FLPexSampler::FLPexSampler()
-  : fEventSize(10000)
-  , fEventRate(1)
-  , fEventCounter(0)
 {
 }
 
@@ -33,59 +30,16 @@ void FLPexSampler::Run()
   LOG(INFO) << ">>>>>>> Run <<<<<<<";
   boost::this_thread::sleep(boost::posix_time::milliseconds(10000));
 
-  boost::thread rateLogger(boost::bind(&FairMQDevice::LogSocketRates, this));
-  boost::thread resetEventCounter(boost::bind(&FLPexSampler::ResetEventCounter, this));
-
-  unsigned long eventId = 1;
-  // int numEvents = 1024;
-
-  void* buffer = operator new[](fEventSize);
-  FairMQMessage* baseMsg = fTransportFactory->CreateMessage(buffer, fEventSize);
+  int sent = 0;
+  int signalValue = 1;
 
   while (fState == RUNNING) {
-    // if (eventId <= numEvents) {
-      FairMQMessage* idPart = fTransportFactory->CreateMessage(sizeof(unsigned long));
-      memcpy(idPart->GetData(), &eventId, sizeof(unsigned long));
+    boost::this_thread::sleep(boost::posix_time::milliseconds(10000));
 
-      fPayloadOutputs->at(0)->Send(idPart, "snd-more");
-      ++eventId;
-
-      FairMQMessage* dataPart = fTransportFactory->CreateMessage();
-      dataPart->Copy(baseMsg);
-
-      if (fPayloadOutputs->at(0)->Send(dataPart, "no-block") == 0) {
-        LOG(ERROR) << "Could not send message with event #" << eventId << " without blocking";
-      }
-
-      // LOG(INFO) << "Sent event #" << eventId;
-      // if (eventId == numEvents) {
-      //   LOG(INFO) << "Sent " << numEvents << " events";
-      // }
-
-      // boost::this_thread::sleep(boost::posix_time::milliseconds(200));
-
-      --fEventCounter;
-
-      while (fEventCounter == 0) {
-        boost::this_thread::sleep(boost::posix_time::milliseconds(1));
-      }
-
-      delete idPart;
-      delete dataPart;
-    // } else {
-    //   boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
-    // }
-  }
-
-  delete baseMsg;
-
-  try {
-    rateLogger.interrupt();
-    rateLogger.join();
-    resetEventCounter.interrupt();
-    resetEventCounter.join();
-  } catch(boost::thread_resource_error& e) {
-    LOG(ERROR) << e.what();
+    if (sent == 0) {
+      FairMQMessage* startSignal = fTransportFactory->CreateMessage(signalValue);
+      sent = fPayloadOutputs->at(0)->Send(startSignal);
+    }
   }
 
   FairMQDevice::Shutdown();
@@ -94,60 +48,4 @@ void FLPexSampler::Run()
   boost::lock_guard<boost::mutex> lock(fRunningMutex);
   fRunningFinished = true;
   fRunningCondition.notify_one();
-}
-
-void FLPexSampler::ResetEventCounter()
-{
-  while (true) {
-    try {
-      fEventCounter = fEventRate / 100;
-      boost::this_thread::sleep(boost::posix_time::milliseconds(10));
-    } catch (boost::thread_interrupted&) {
-      break;
-    }
-  }
-}
-
-void FLPexSampler::SetProperty(const int key, const string& value, const int slot /*= 0*/)
-{
-  switch (key) {
-    default:
-      FairMQDevice::SetProperty(key, value, slot);
-      break;
-  }
-}
-
-string FLPexSampler::GetProperty(const int key, const string& default_ /*= ""*/, const int slot /*= 0*/)
-{
-  switch (key) {
-    default:
-      return FairMQDevice::GetProperty(key, default_, slot);
-  }
-}
-
-void FLPexSampler::SetProperty(const int key, const int value, const int slot /*= 0*/)
-{
-  switch (key) {
-    case EventSize:
-      fEventSize = value;
-      break;
-    case EventRate:
-      fEventRate = value;
-      break;
-    default:
-      FairMQDevice::SetProperty(key, value, slot);
-      break;
-  }
-}
-
-int FLPexSampler::GetProperty(const int key, const int default_ /*= 0*/, const int slot /*= 0*/)
-{
-  switch (key) {
-    case EventSize:
-      return fEventSize;
-    case EventRate:
-      return fEventRate;
-    default:
-      return FairMQDevice::GetProperty(key, default_, slot);
-  }
 }
